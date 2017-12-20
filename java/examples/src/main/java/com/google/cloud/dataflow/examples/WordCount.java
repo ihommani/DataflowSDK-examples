6d2c5354c870560120464747e7363d41882d8dc0
ihommani <ismaelhommani@gmail.com>
2017-12-20 21:34:08 +0100

    Reorder methods

--- java/examples/src/main/java/com/google/cloud/dataflow/examples/WordCount.java
+++ java/examples/src/main/java/com/google/cloud/dataflow/examples/WordCount.java
@@ -81,65 +81,19 @@ import org.apache.beam.sdk.values.PCollection;
  */
 public class WordCount {
 
-    /**
-     * Concept #2: You can make your pipeline assembly code less verbose by defining your DoFns
-     * statically out-of-line. This DoFn tokenizes lines of text into individual words; we pass it
-     * to a ParDo in the pipeline.
-     */
-    static class ExtractWordsFn extends DoFn<String, String> {
-        private final Counter emptyLines = Metrics.counter(ExtractWordsFn.class, "emptyLines");
+    public static void main(String[] args) {
+        WordCountOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+                .as(WordCountOptions.class);
+        Pipeline p = Pipeline.create(options);
 
-        @ProcessElement
-        public void processElement(ProcessContext c) {
-            if (c.element().trim().isEmpty()) {
-                emptyLines.inc();
-            }
+        // Concepts #2 and #3: Our pipeline applies the composite CountWords transform, and passes the
+        // static FormatAsTextFn() to the ParDo transform.
+        p.apply("ReadLines", TextIO.read().from(options.getInputFile()))
+                .apply(new CountWords())
+                .apply(MapElements.via(new FormatAsTextFn()))
+                .apply("WriteCounts", TextIO.write().to(options.getOutput()));
 
-            // Split the line into words.
-            String[] words = c.element().split(ExampleUtils.TOKENIZER_PATTERN);
-
-            // Output each word encountered into the output PCollection.
-            for (String word : words) {
-                if (!word.isEmpty()) {
-                    c.output(word);
-                }
-            }
-        }
-    }
-
-    /**
-     * A SimpleFunction that converts a Word and Count into a printable string.
-     */
-    public static class FormatAsTextFn extends SimpleFunction<KV<String, Long>, String> {
-        @Override
-        public String apply(KV<String, Long> input) {
-            return input.getKey() + ": " + input.getValue();
-        }
-    }
-
-    /**
-     * A PTransform that converts a PCollection containing lines of text into a PCollection of
-     * formatted word counts.
-     * <p>
-     * <p>Concept #3: This is a custom composite transform that bundles two transforms (ParDo and
-     * Count) as a reusable PTransform subclass. Using composite transforms allows for easy reuse,
-     * modular testing, and an improved monitoring experience.
-     */
-    public static class CountWords extends PTransform<PCollection<String>,
-            PCollection<KV<String, Long>>> {
-        @Override
-        public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
-
-            // Convert lines of text into individual words.
-            PCollection<String> words = lines.apply(
-                    ParDo.of(new ExtractWordsFn()));
-
-            // Count the number of times each word occurs.
-            PCollection<KV<String, Long>> wordCounts =
-                    words.apply(Count.<String>perElement());
-
-            return wordCounts;
-        }
+        p.run().waitUntilFinish();
     }
 
     /**
@@ -173,18 +127,65 @@ public class WordCount {
         void setOutput(String value);
     }
 
-    public static void main(String[] args) {
-        WordCountOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
-                .as(WordCountOptions.class);
-        Pipeline p = Pipeline.create(options);
+    /**
+     * A PTransform that converts a PCollection containing lines of text into a PCollection of
+     * formatted word counts.
+     * <p>
+     * <p>Concept #3: This is a custom composite transform that bundles two transforms (ParDo and
+     * Count) as a reusable PTransform subclass. Using composite transforms allows for easy reuse,
+     * modular testing, and an improved monitoring experience.
+     */
+    public static class CountWords extends PTransform<PCollection<String>, PCollection<KV<String, Long>>> {
+        @Override
+        public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
 
-        // Concepts #2 and #3: Our pipeline applies the composite CountWords transform, and passes the
-        // static FormatAsTextFn() to the ParDo transform.
-        p.apply("ReadLines", TextIO.read().from(options.getInputFile()))
-                .apply(new CountWords())
-                .apply(MapElements.via(new FormatAsTextFn()))
-                .apply("WriteCounts", TextIO.write().to(options.getOutput()));
+            // Convert lines of text into individual words.
+            PCollection<String> words = lines.apply(
+                    ParDo.of(new ExtractWordsFn()));
 
-        p.run().waitUntilFinish();
+            // Count the number of times each word occurs.
+            PCollection<KV<String, Long>> wordCounts =
+                    words.apply(Count.<String>perElement());
+
+            return wordCounts;
+        }
     }
+
+    /**
+     * A SimpleFunction that converts a Word and Count into a printable string.
+     */
+    public static class FormatAsTextFn extends SimpleFunction<KV<String, Long>, String> {
+        @Override
+        public String apply(KV<String, Long> input) {
+            return input.getKey() + ": " + input.getValue();
+        }
+    }
+
+
+    /**
+     * Concept #2: You can make your pipeline assembly code less verbose by defining your DoFns
+     * statically out-of-line. This DoFn tokenizes lines of text into individual words; we pass it
+     * to a ParDo in the pipeline.
+     */
+    static class ExtractWordsFn extends DoFn<String, String> {
+        private final Counter emptyLines = Metrics.counter(ExtractWordsFn.class, "emptyLines");
+
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            if (c.element().trim().isEmpty()) {
+                emptyLines.inc();
+            }
+
+            // Split the line into words.
+            String[] words = c.element().split(ExampleUtils.TOKENIZER_PATTERN);
+
+            // Output each word encountered into the output PCollection.
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    c.output(word);
+                }
+            }
+        }
+    }
+
 }
